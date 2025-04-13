2720de6274bb2eb27066157acd261a912ac7b5a0
Janniche Aarøen <jannicheaaroen@gmail.com>
2025-04-14 01:55:47 +0200

    Første commit

---
+++ src/components/Meme.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+export default function Meme() {
+  const [allMeme, setAllMeme] = React.useState({});
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMeme(data.data.memes));
+  }, []);
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImg: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
+  function getMemeImg() {
+    const randomIndex = Math.floor(Math.random() * allMeme.length);
+    const randomImg = allMeme[randomIndex].url;
+
+    setMeme(function (prevMeme) {
+      return {
+        ...prevMeme,
+        randomImg: randomImg,
+      };
+    });
+  }
+  return (
+    <main className="content">
+      <div className="div-inputs">
+        <div className="input-display">
+          <label htmlFor="input-top">Tekst øverst</label>
+          <input
+            type="text"
+            id="input-top"
+            name="topText"
+            placeholder="Shut up"
+            value={meme.topText}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-display">
+          <label htmlFor="input-bottom">Tekst nederst</label>
+          <input
+            type="text"
+            id="input-bottom"
+            name="bottomText"
+            placeholder="and take my money"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <button onClick={getMemeImg}>Get a new meme image 🖼</button>
+      <div className="img-container">
+        <img className="meme-img" src={meme.randomImg} />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <nav>
+      <div className="nav-left-side">
+        <img className="troll-face" src="./troll-face.png" />
+        <h1>Meme generator</h1>
+      </div>
+      <p>React course - Project 3</p>
+    </nav>
+  );
+}
